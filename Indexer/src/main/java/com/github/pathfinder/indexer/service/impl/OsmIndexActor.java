296ddc4110f4e1b515f3c6039715bac51a3c1ced
Daniil Kamakin <cursedowguy@gmail.com>
2024-01-23 13:55:34 +0100

    search boarder chunks instead of points when connecting, so we speed up the indexing

--- Indexer/src/main/java/com/github/pathfinder/indexer/service/impl/OsmIndexActor.java
+++ Indexer/src/main/java/com/github/pathfinder/indexer/service/impl/OsmIndexActor.java
@@ -1,7 +1,6 @@
 package com.github.pathfinder.indexer.service.impl;
 
 import com.github.pathfinder.core.aspect.Logged;
-import com.github.pathfinder.core.executor.PlatformExecutor;
 import com.github.pathfinder.core.tools.IDateTimeSupplier;
 import com.github.pathfinder.indexer.configuration.IndexerRetryConfiguration;
 import com.github.pathfinder.indexer.database.entity.IndexBoxEntity;
@@ -9,12 +8,10 @@ import com.github.pathfinder.indexer.service.BoxSearcherService;
 import com.github.pathfinder.indexer.service.osm.impl.OsmIndexTask;
 import com.github.pathfinder.searcher.api.SearcherApi;
 import com.github.pathfinder.searcher.api.data.ConnectChunkMessage;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.CollectionUtils;
 
 @Slf4j
 @Component
@@ -25,27 +22,13 @@ public class OsmIndexActor {
     private final BoxSearcherService        boxSearcherService;
     private final IDateTimeSupplier         dateTimeSupplier;
     private final OsmIndexTask              task;
-    private final PlatformExecutor          executor;
     private final IndexerRetryConfiguration retryConfiguration;
 
     @Logged
     @Transactional
     public void perform() {
-        boxSearcherService.savable(retryConfiguration.getSaveDelay()).forEach(this::save);
-        connect(boxSearcherService.connectable(retryConfiguration.getConnectDelay()));
-    }
-
-    private void save(IndexBoxEntity box) {
-        executor.submit(() -> task.accept(box));
-    }
-
-    private void connect(List<IndexBoxEntity> boxes) {
-        if (CollectionUtils.isEmpty(boxes)) {
-            log.info("No boxes for connection");
-            return;
-        }
-
-        boxes.forEach(this::connect);
+        boxSearcherService.savable(retryConfiguration.getSaveDelay()).forEach(task::accept);
+        boxSearcherService.connectable(retryConfiguration.getConnectDelay()).forEach(this::connect);
     }
 
     private void connect(IndexBoxEntity box) {
