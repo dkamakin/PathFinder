9267f0c0dff865f9f7de7e72ccf051f7f127e7e0
Daniil Kamakin <cursedowguy@gmail.com>
2024-01-28 14:43:11 +0100

    remaster the indexer boxes

--- Core/src/main/java/com/github/pathfinder/core/data/Coordinate.java
+++ Core/src/main/java/com/github/pathfinder/core/data/Coordinate.java
@@ -1,5 +1,6 @@
 package com.github.pathfinder.core.data;
 
+import com.google.common.base.Objects;
 import lombok.experimental.UtilityClass;
 
 public record Coordinate(double latitude, double longitude) {
@@ -12,6 +13,26 @@ public record Coordinate(double latitude, double longitude) {
         public static final String LATITUDE_MAX_VALUE_STRING  = "90";
         public static final String LATITUDE_MIN_VALUE_STRING  = "-90";
 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Coordinate that = (Coordinate) o;
+        return Double.compare(latitude, that.latitude) == 0 &&
+                Double.compare(longitude, that.longitude) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(latitude, longitude);
     }
 
 }
